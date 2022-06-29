7ce1608edd51c79c23a00ad396c5466d021277af
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-06-29 12:43:00 +0530

    init

--- Arrays/04.cpp
+++ Arrays/04.cpp
@@ -1,4 +1,27 @@
-
+//More Optimal Solution
+    void sort012(int a[], int n)
+    {
+        int low=0, mid=0, high=n-1;
+        while(mid<=high ){
+            if(a[mid]==0){
+                int temp=a[low];
+                a[low]=a[mid];
+                a[mid]=temp;
+                low++;mid++;
+            }
+            else if(a[mid]==1){
+                mid++;
+            }
+            
+            else if(a[mid]==2){
+                int temp=a[high];
+                a[high]=a[mid];
+                a[mid]=temp;
+                high--;
+            }
+        }
+    }
+    
 // Simple Solution
     void sort012(int a[], int n)
     {
