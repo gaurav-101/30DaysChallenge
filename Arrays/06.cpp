f73e9edef145a3f6639a0032c4099380f0f6dea7
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-06-29 13:54:10 +0530

    init

--- Arrays/06.cpp
+++ Arrays/06.cpp
@@ -1,3 +1,21 @@
+//Intersection of two arrays
+public void intersectionOfTwoUnSortedArray(int[] arr1, int[] arr2, Set<Integer> finalIntersection) {
+    int i = 0;
+    int j = 0;
+    Set<Integer> tempSet = new HashSet<>();
+
+    while (i < arr1.length) {
+      tempSet.add(arr1[i++]);
+    }
+
+    while (j < arr2.length) {
+      if (tempSet.contains(arr2[j])) {
+        finalIntersection.add(arr2[j]);
+      }
+
+      j++;
+    }
+
 
 
 //Union of two Arrays
