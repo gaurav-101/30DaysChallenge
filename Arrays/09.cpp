56e67f60d11d23573aa40958cd303b960903bf84
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-06-30 00:28:51 +0530

    init

---
+++ Arrays/09.cpp
@@ -0,0 +1,15 @@
+int getMinDiff(int arr[], int n, int k) {
+    sort(arr,arr+n);
+    int ans=arr[n-1]-arr[0];
+    int smallest=arr[0]+k;
+    int largest=arr[n-1]-k;
+    int mi,ma;
+    for(int i=0;i<n;i++){
+        ma=max(largest,arr[i]+k);
+        mi=min(smallest,arr[i+1]-k);
+        if(mi<0)
+            continue;
+        ans=min(ans,ma-mi);
+    }
+    return ans;
+}
