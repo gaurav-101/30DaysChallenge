a573ab99c330dea9baaf3816fbb177747bc3de9f
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-06-29 20:15:53 +0530

    init

---
+++ Arrays/08.cpp
@@ -0,0 +1,18 @@
+//Kadane's Algo
+
+long long maxSubarraySum(int arr[], int n){
+    
+    long long maxSum=arr[0], prevSum=arr[0];
+    for(int i=1;i<n;i++){
+        if(prevSum<0){
+            prevSum=arr[i];
+        }
+        else{
+            prevSum+=arr[i];
+        }
+        if(maxSum<prevSum){
+            maxSum=prevSum;
+        }
+    }
+    return maxSum;
+}
