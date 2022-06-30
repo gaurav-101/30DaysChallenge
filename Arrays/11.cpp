b53a48b373afe79ac64fe348fa6385bc88ab59c6
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-06-30 11:43:51 +0530

    mthod1

---
+++ Arrays/11.cpp
@@ -0,0 +1,12 @@
+// Method-1 O(nlogn)
+
+int findDuplicate(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int n=nums.size();
+    for(int i=0;i<n-1;i++){
+        if(nums[i]==nums[i+1]){
+            return nums[i];
+        }
+    }
+    return -1;
+}
