84fb6f4b9104364364db0e0eda59b28d80c70b49
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-06-30 12:02:58 +0530

    init

--- Arrays/11.cpp
+++ Arrays/11.cpp
@@ -1,5 +1,20 @@
-
-
+// Method-3 Most optimise approach
+//Linked list Cycle
+int findDuplicate(vector<int>& nums) {
+        int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        
+        fast=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+}
 
 
 
