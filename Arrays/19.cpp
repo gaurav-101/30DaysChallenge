7590d03b40941369e6dcf2ef2c4c381ee5f3a706
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-03 19:01:16 +0530

    init

--- Arrays/19.cpp
+++ Arrays/19.cpp
@@ -1,4 +1,29 @@
-//Method-1
+//Method -2 Using three pointers
+vector <int> commonElements (int a[], int b[], int c[], int n1, int n2, int n3)
+        {
+            int i=0,j=0,k=0;
+            vector <int> ans;
+            while(i<n1 and j<n2 and k<n3){
+                if(a[i]==b[j] and b[j]==c[k]){
+                    ans.push_back(a[i]);
+                    i++;j++;k++;
+                }
+                else if( a[i]<b[j]) i++;
+                else if( b[j]<c[k]) j++;
+                else k++;
+                int xx=a[i-1];
+                while(a[i]==xx) i++;
+                int yy=b[j-1];
+                while(b[j]==yy) j++;
+                int zz=c[k-1];
+                while(c[k]==zz) k++;
+            }
+            
+            return ans;
+        }
+
+
+//Method-1  Using map
 vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
 {
     vector <int> ans;
