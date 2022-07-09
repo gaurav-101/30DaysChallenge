19c266e58a19d02826114212c236b471a67c02e5
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-10 01:00:03 +0530

    method1

---
+++ Strings/01.cpp
@@ -0,0 +1,12 @@
+// Using two pointer
+void reverseString(vector<char>& s) {
+        int start=0;
+        int end=s.size()-1;
+        while(start<end){
+            char temp=s[end];
+            s[end]=s[start];
+            s[start]=temp;
+            start++;
+            end--;
+        }
+    }
