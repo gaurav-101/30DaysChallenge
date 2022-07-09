5532a52bc5b0373b897a31a9fe2741a221dc2bc7
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-10 01:00:56 +0530

    method2

--- Strings/01.cpp
+++ Strings/01.cpp
@@ -10,3 +10,18 @@ void reverseString(vector<char>& s) {
             end--;
         }
     }
+
+// Using Recursion
+void ans(vector<char>& s, int st, int n){ 
+    if(st>n-st-1){
+        return;
+    }
+    swap(s[st],s[n-st-1]);
+    st++;
+    return ans(s, st, n);
+}
+void reverseString(vector<char>& s) {
+    int n=s.size();
+    int st=0;
+    ans(s,st,n);
+}
