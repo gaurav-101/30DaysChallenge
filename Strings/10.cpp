f6b8951f39c02fddfbccf04e39753d2c38fcdae9
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-10 16:44:32 +0530

    init

--- Strings/10.cpp
+++ Strings/10.cpp
@@ -1,3 +1,28 @@
+//Method-2   // Using Recursion
+void solve(vector<string>& ans, string str, string output, int i) {
+    //base case
+    if(i>=str.length()) {
+        if(output.length()>0)
+        ans.push_back(output);
+        return ;
+        }
+    
+    //exclude
+    solve(ans, str, output, i+1);
+    //include
+    output.push_back(str[i]);
+    solve(ans, str, output, i+1);
+}
+
+vector<string> subsequences(string str){
+	
+	vector<string> ans;
+    string output = "";
+    solve(ans,str,output,0);
+    return ans;
+	
+}
+
 
 //   Method -1 // Using Bit
 void printSS(string ques, string ans){
