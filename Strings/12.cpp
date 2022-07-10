a235672aba6e2f24240bb96cfbe4b5660eb87e3f
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-10 14:06:21 +0530

    init

--- Strings/12.cpp
+++ Strings/12.cpp
@@ -1,21 +1,21 @@
 int maxSubStr(string str){
     int ans=0;
-    int count=1;
-    char prev=str[0];
-    int i=1;
+    int count0=0;
+    int count1=0;
+    int i=0;
     while(i<str.length()){
-        if(str[i]==prev){
-            count++;
+        if(str[i]=='0'){
+            count0++;
         }
         else{
-            count--;
+            count1++;
         }
-        if(count==0){
+        if(count0==count1){
             ans++;
         }
         i++;
     }
-    if(count!=0){
+    if(count0!=count1){
         return -1;
     }
     return ans;
