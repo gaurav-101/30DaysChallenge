705da216bfc44190332a4efdb867def7d0f7f321
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-11 17:24:23 +0530

    init

--- SearchingAndSorting/04.cpp
+++ SearchingAndSorting/04.cpp
@@ -1,3 +1,25 @@
+//Binary Search Question 1
+long long int floorSqrt(long long int x) 
+{
+    long long int i=1, j=x;
+    long long int ans;
+    while(i<=j){
+        long long int mid=(i+j)/2;
+        if(mid*mid==x){
+            return mid;
+        }
+        else if(mid*mid<x){
+            ans=mid;
+            i=mid+1;
+        }
+        else{
+            j=mid-1;
+        }
+    }
+    return ans;
+}
+
+// Question 2
 int countSquares(int N) {
     int ans=0;
     for(int i=1;i*i<=N;i++){
