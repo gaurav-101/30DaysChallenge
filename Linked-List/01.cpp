e762e6d738f998bf64872a4b069bb03c6b276ef6
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-15 15:41:33 +0530

    method2

--- Linked-List/01.cpp
+++ Linked-List/01.cpp
@@ -1,3 +1,5 @@
+// Iterative way
+
 struct Node* reverseList(struct Node *head)
 {
     struct Node* prev=NULL;
@@ -12,4 +14,25 @@ struct Node* reverseList(struct Node *head)
     }
     return prev;
     
+}
+
+//Recursive way
+
+struct Node* solve(struct Node *prev, struct Node *curr){
+    if(curr==NULL){
+        return prev;
+    }
+    struct Node *forward=curr->next;
+    curr->next=prev;
+    prev=curr;
+    curr=forward;
+    solve(prev,curr);
+}
+struct Node* reverseList(struct Node *head)
+{
+    struct Node* prev=NULL;
+    struct Node* curr=head;
+    struct Node* forward;
+    
+    return solve(prev,curr);
 }
