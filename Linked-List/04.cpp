e44731120411cd72fe2dd6c26efd7f11f5e4f6e3
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-15 20:10:31 +0530

    init

---
+++ Linked-List/04.cpp
@@ -0,0 +1,34 @@
+void removeLoop(Node* head)
+{
+    Node* high=head;
+    Node* low=head;
+    while(high!=NULL and high->next != NULL){
+        high=high->next->next;
+        low=low->next;
+        if(high==low){
+            break;
+        }
+    }
+    high=head;
+    //exception when low==high = head
+    if(low==head){
+        while(high!=NULL){
+            if((high->next)==(low)){
+                high->next=NULL;
+                return;
+            }
+            high=high->next;
+        }
+    }
+    else{
+        while(high!=NULL and low!=NULL){
+            if((high->next)==(low->next)){
+                low->next=NULL;
+                return;
+            }
+            high=high->next;
+            low=low->next;
+        }
+    }
+    return;
+}
