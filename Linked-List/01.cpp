e22ef5a2285f53ec08451deef0ab324ae90c1222
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-16 21:27:58 +0530

    init

--- Linked-List/01.cpp
+++ Linked-List/01.cpp
@@ -36,3 +36,16 @@ struct Node* reverseList(struct Node *head)
     
     return solve(prev,curr);
 }
+// Method-3
+void reverseLL(Node* &head){
+    Node* prev=NULL;
+    Node* curr=head;
+    Node* forward;
+    while(curr!=NULL){
+        forward=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=forward;
+    }
+    head=prev;
+}
