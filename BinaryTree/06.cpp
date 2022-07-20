2e823c781dfd1c9e9bbb007d839d6932b248bc34
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-20 13:25:40 +0530

    init

--- BinaryTree/06.cpp
+++ BinaryTree/06.cpp
@@ -1,14 +1,36 @@
-void solve(Node* root, vector<int>& ans){
-        if(root==NULL){
-            return;
+//Iterative way
+vector<int> inOrder(Node* node) {
+    vector<int> ans;
+    stack<Node*> st;
+    while(true){
+        if(node!=NULL){
+            st.push(node);
+            node=node->left;
         }
-        solve(root->left,ans);
-        ans.push_back(root->data);
-        solve(root->right,ans);
+        else{
+            if(st.empty()) break;
+            node=st.top();
+            st.pop();
+            ans.push_back(node->data);
+            node=node->right;
+        }
+    }
+    return ans;
+}
+
+//Recurive way
+
+void solve(Node* root, vector<int>& ans){
+    if(root==NULL){
         return;
     }
-    vector<int> inOrder(Node* root) {
-        vector <int> ans;
-        solve(root,ans);
-        return ans;
-    }
+    solve(root->left,ans);
+    ans.push_back(root->data);
+    solve(root->right,ans);
+    return;
+}
+vector<int> inOrder(Node* root) {
+    vector <int> ans;
+    solve(root,ans);
+    return ans;
+}
