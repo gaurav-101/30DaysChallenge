43aa7d8ca108b39b2133c13da59524c1571fb0d2
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-20 15:17:53 +0530

    init

--- BinaryTree/07.cpp
+++ BinaryTree/07.cpp
@@ -20,3 +20,14 @@ vector <int> preorder(Node* curr)
     return preOrder;
 
 }
+
+// Recursive method
+void solve(Node* root, vector<int>& ans){
+    if(root==NULL){
+        return;
+    }
+    ans.push_back(root->data);
+    solve(root->left,ans);
+    solve(root->right,ans);
+    return;
+}
