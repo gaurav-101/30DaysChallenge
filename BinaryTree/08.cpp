7e0a487c4978c2938343832d41848500603eb7f2
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-20 16:04:26 +0530

    init

--- BinaryTree/08.cpp
+++ BinaryTree/08.cpp
@@ -1,3 +1,30 @@
+//Iterative Way using two stack
+vector <int> postOrder(Node* root)
+{
+    Node* node=root;
+    vector < int > postOrder;
+    stack<Node*> s1,s2;
+    s1.push(node);
+    while(!s1.empty()){
+        node=s1.top();
+        s1.pop();
+        s2.push(node);
+        
+        if(node->left){
+            s1.push(node->left);
+        }
+        if(node->right){
+            s1.push(node->right);
+        }
+    }
+    while(!s2.empty()){
+        postOrder.push_back((s2.top())->data);
+        s2.pop();
+    }
+    return postOrder;
+}
+
+
 //Recursive way
 void solve(Node* root, vector<int>& ans){
     if(root==NULL){
