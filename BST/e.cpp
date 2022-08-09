8c0b6076dd38e158d90356842b62f52681a07a62
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-08-10 00:55:25 +0530

    comment

--- BST/e.cpp
+++ BST/e.cpp
@@ -1,19 +1,24 @@
 int solve(Node* root, int k,int& i){
+    //Base case
     if(root==NULL){
         return -1;
     }
     
+    //firstly we will check the left part of tree
     int left=solve(root->left,k,i);
+    //if we will get the kth element in left part then we will return it
     if(left!=-1){
         return left;
     }
     
+    //  we are incrementing i
     i++;
     if(i==k){
         int ans=root->data;
         return ans;
     }
     
+    //at last we will check on right side
     return solve(root->right,k,i);
 }
 
