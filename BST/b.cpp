0501e74564bc487653ee590c5aa41114bf055428
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-08-09 16:01:48 +0530

    init

--- BST/b.cpp
+++ BST/b.cpp
@@ -1,3 +1,15 @@
+//Iterative Method
+bool search(Node* root, int x) {
+    Node* temp=root;
+    while(root!=NULL){
+        if(root->data==x) return true;
+        else if(root->data>x) root=root->left;
+        else root=root->right;
+    }
+    return false;
+}
+
+//Recursive method
 bool solve (Node* root, int x){
     if(root==NULL){
         return false;
