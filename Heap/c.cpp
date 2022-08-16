5803650fbb015335783db43a30d69068f456717e
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-08-16 11:16:16 +0530

    init

---
+++ Heap/c.cpp
@@ -0,0 +1,30 @@
+void heapify(vector<int>& arr, int n, int i){
+    int largest=i;
+    int left=2*i+1;
+    int right=2*i+2;
+    if(left<n and arr[largest]<arr[left]){
+        largest=left;
+    }
+    if(right<n and arr[largest]<arr[right]){
+        largest=right;
+    }
+    if(largest!=i){
+        swap(arr[largest],arr[i]);
+        heapify(arr,n,largest);
+    }
+}
+
+vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+    vector<int> ans;
+    for(int i=0;i<n;i++){
+        ans.push_back(a[i]);
+    }
+    for(int i=0;i<m;i++){
+        ans.push_back(b[i]);
+    }
+    int size=ans.size();
+    for(int i=size/2 -1;i>=0;i--)
+        heapify(ans,size,i);
+        
+    return ans;
+}
