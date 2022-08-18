cd03fedce3467075a431545a24f80a0b4733eee3
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-08-19 01:00:02 +0530

    init

--- Heap/g.cpp
+++ Heap/g.cpp
@@ -1,3 +1,27 @@
+//Iterative Way
+Node * mergeKLists(Node *arr[], int k)
+{
+    priority_queue<Node*, vector<Node*>, compare> mini;
+    for(int i=0;i<k;i++){
+        mini.push(arr[i]);
+    }
+    
+    Node* root=mini.top();
+    Node* temp=root;
+    mini.pop();
+    while(!mini.empty()){
+        if(temp->next!=NULL){
+            mini.push(temp->next);
+        }
+        temp->next=mini.top();
+        mini.pop();
+        temp=temp->next;
+    }
+    return root;
+    
+}
+
+
 // Recursive Way
 class compare{
     public:
