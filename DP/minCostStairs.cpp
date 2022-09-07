4b2739f76f68086b0a1612d700944e5bed942566
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-09-08 01:36:11 +0530

    init

--- DP/minCostStairs.cpp
+++ DP/minCostStairs.cpp
@@ -1,3 +1,21 @@
+//Method - 3
+int solve(vector<int> cost, int N){
+    
+    int prev1=0;
+    int prev2=0;
+    for(int i=2; i<=N ; i++){
+        int curr=min(prev1+cost[i-1], prev2+cost[i-2]);
+        prev2=prev1;
+        prev1=curr;
+    }
+    return prev1;
+}
+
+int minCostClimbingStairs(vector<int>&cost ,int N) {
+    return solve(cost, N);
+}
+
+
 //Method - 2
 int solve(vector<int> cost, int N, vector<int> &dp){
     
