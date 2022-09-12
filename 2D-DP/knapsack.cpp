b3695a5c3ad62e60c4bdde087c27fc837c612c9d
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-09-12 23:22:52 +0530

    3

--- 2D-DP/knapsack.cpp
+++ 2D-DP/knapsack.cpp
@@ -1,3 +1,38 @@
+// Method-3 tabulation
+int solve(int wt[], int val[], int n, int capacity){
+    
+    vector<vector<int>> dp(n, vector<int> (capacity+1, 0));
+    
+    for(int w=wt[0]; w<=capacity; w++){
+        if(wt[0]<=capacity){
+            dp[0][w]=val[0];
+        }
+        else{
+            dp[0][w]=0;
+        }
+    }
+    
+    for(int index=1; index<n; index++){
+        for(int w=0; w<=capacity; w++){
+            int include=0;
+            if(wt[index]<=w){
+                include=val[index] + dp[index-1][w-wt[index] ];
+            }
+            
+            int exclude=dp[index-1][w];
+
+            dp[index][w]=max(include, exclude);
+        }
+    }
+    
+    return dp[n-1][capacity]; 
+}
+
+int knapSack(int w, int wt[], int val[], int n) 
+{ 
+    return solve(wt, val, n, w);
+}
+
 // Method -2 dp and memoization
 int solve(int wt[], int val[], int index, int capacity, vector<vector<int>> dp){
     if(index==0){
